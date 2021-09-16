f80ac91f87997d26a02d677ce4870455956d05e4
Rachel Hendricks <rachelhen92@gmail.com>
2021-09-16 16:40:39 -0600

    Add basic styling to PlanetInfo component

--- src/components/PlanetInfo/PlanetInfo.js
+++ src/components/PlanetInfo/PlanetInfo.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './PlanetInfo.css';
 
-const PlanetInfo = ({ date, explanation, title, image}) => {
+const PlanetInfo = ({ date, explanation, title, image }) => {
   return (
-    <article>
+    <article className='card'>
       <h1>{title}</h1>
-      <p>{date}</p>
+      <p>{new Date(date).toDateString()}</p>
       <img src={image} alt={title} />
       <p>{explanation}</p>
     </article>
