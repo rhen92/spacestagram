43f55487a02fefff17cc5db2b8c28480911d8242
Rachel Hendricks <rachelhen92@gmail.com>
2021-09-21 14:51:53 -0600

    Update api-calls to be dynamic

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,4 +1,10 @@
+let today = new Date();
+today = today.toISOString().split('T')[0];
+let monthBefore = new Date().setDate(new Date().getDate() - 30)
+monthBefore = new Date(monthBefore)
+monthBefore = monthBefore.toISOString().split('T')[0];
+
 export const getPlanetPics = () => {
-  return fetch('https://api.nasa.gov/planetary/apod?&api_key=TpTMyPm4uHLzn8tCrq33k7g72lI3PgeIPepDJ8wf&start_date=2021-08-16&end_date=2021-09-16')
+  return fetch(`https://api.nasa.gov/planetary/apod?&api_key=TpTMyPm4uHLzn8tCrq33k7g72lI3PgeIPepDJ8wf&start_date=${monthBefore}&end_date=${today}`)
     .then(response => response.json())
 }
