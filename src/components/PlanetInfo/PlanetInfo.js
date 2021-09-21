f62c204727e2b4dd6e26f4633363bd619bfd5edb
Rachel Hendricks <rachelhen92@gmail.com>
2021-09-21 15:17:28 -0600

    Add like feature with heart icons

--- src/components/PlanetInfo/PlanetInfo.js
+++ src/components/PlanetInfo/PlanetInfo.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './PlanetInfo.css';
+import { RiHeart3Line, RiHeart3Fill } from 'react-icons/ri';
 
-const PlanetInfo = ({ date, media, title, url }) => {
-  return (
-    <article className='card'>
-      <h1>{title}</h1>
-      <p>{new Date(date).toDateString()}</p>
-      {media === 'video' ? <a href={url} target='_blank' rel='noreferrer'>Click to see video</a> : <img src={url} alt={title} />}
-    </article>
-  )
+class PlanetInfo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      like: false
+    }
+  }
+
+  likePicture = () => {
+    this.setState({like: !this.state.like})
+  }
+
+  render() {
+    return (
+      <article className='card'>
+      <h1>{this.props.title}</h1>
+      <p>{new Date(this.props.date).toDateString()}</p>
+      {this.props.media === 'video' ? <a href={this.props.url} target='_blank' rel='noreferrer'>Click to see video</a> : <img src={this.props.url} alt={this.props.title} />}
+      {!this.state.like ? <RiHeart3Line className='heart' onClick={() => this.likePicture()} /> : <RiHeart3Fill className='heart' onClick={() => this.likePicture()} />}
+      </article>
+
+    )
+  }
 }
+
 
 export default PlanetInfo;
